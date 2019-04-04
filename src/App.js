aa2ea858568a4c7df44f70c392fc9dd05b1a426b
chengjiang <15712859926@163.com>
2019-04-04 13:59:56 +0800

    添加电影详情页面

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import Movies from "./components/movies";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import NotFound from "./components/notFound";
+import NavBar from "./components/NavBar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect exact from="/" to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
