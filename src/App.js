616d8208aca50b92b1f96bbb549a464cb0096fc9
chengjiang <15712859926@163.com>
2019-04-04 15:37:53 +0800

    表单验证

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import NavBar from "./components/NavBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,6 +29,9 @@ const NavBar = () => {
           <NavLink className="nav-link" to="/rentals">
             Rentals
           </NavLink>
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
         </ul>
       </div>
     </nav>
