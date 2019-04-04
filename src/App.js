56ec44fd15f3e8d0be9351a750bbcaf79bd47924
chengjiang <15712859926@163.com>
2019-04-04 11:39:45 +0800

    添加导航路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
 import "./App.css";
-
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
